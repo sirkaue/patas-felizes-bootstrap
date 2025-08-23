c16c9c9b24cb1bf9a891e25c2d9bdf88bb8ce8c7
kauatwn <kaualopescontato@gmail.com>
2025-08-23 10:23:08 -0300

    feat: add main.js to initialize Bootstrap and Lucide icons

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import {
+  createIcons,
+  HandHeart,
+  Heart,
+  Mail,
+  MapPin,
+  Menu,
+  NotebookPen,
+  PawPrint,
+  Phone,
+  ShieldCheck,
+  UserRound,
+} from "lucide";
+
+createIcons({
+  icons: {
+    PawPrint,
+    Menu,
+    Heart,
+    HandHeart,
+    NotebookPen,
+    ShieldCheck,
+    UserRound,
+    Mail,
+    Phone,
+    MapPin,
+  },
+});
