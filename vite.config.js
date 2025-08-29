1e838605ab8dfdd866ff315368b33eca51de800b
kauatwn <kaualopescontato@gmail.com>
2025-08-29 15:27:08 -0300

    refactor: update output directory for build artifacts

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: resolve(__dirname, "src"),
   publicDir: resolve(__dirname, "public"),
   build: {
-    outDir: "dist",
+    outDir: "../dist",
   },
   css: {
     preprocessorOptions: {
