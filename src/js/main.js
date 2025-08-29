70d5fb6915af0386ab2014bb0411c866093b7661
kauatwn <kaualopescontato@gmail.com>
2025-08-29 15:36:06 -0300

    feat: initialize testimonials on DOMContentLoaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 import "../scss/styles.scss";
 import { bootstrapInit } from "./bootstrapInit";
 import { lucideInit } from "./lucideInit";
+import { testimonialsInit } from "./testimonialsInit";
 
 function app() {
   document.addEventListener("DOMContentLoaded", () => {
     bootstrapInit();
     lucideInit();
+    testimonialsInit();
   });
 }
 
